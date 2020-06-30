31371109d9963d8ad881324973dd34be1a2a4c71
fengchuan <1299693999@qq.com>
2020-06-30 18:41:48 +0800

    very good

---
+++ injectconsole/dllmain.cpp
@@ -0,0 +1,70 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <iostream>
+#include<tchar.h>
+#include <Python.h>
+void CreateConsole()
+{
+    if (!AllocConsole()) {
+        // Add some error handling here.
+        // You can call GetLastError() to get more info about the error.
+        return;
+    }
+
+    // std::cout, std::clog, std::cerr, std::cin
+    FILE* fDummy;
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+    std::cout.clear();
+    std::clog.clear();
+    std::cerr.clear();
+    std::cin.clear();
+
+    // std::wcout, std::wclog, std::wcerr, std::wcin
+    HANDLE hConOut = CreateFile(_T("CONOUT$"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hConIn = CreateFile(_T("CONIN$"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
+    SetStdHandle(STD_ERROR_HANDLE, hConOut);
+    SetStdHandle(STD_INPUT_HANDLE, hConIn);
+    std::wcout.clear();
+    std::wclog.clear();
+    std::wcerr.clear();
+    std::wcin.clear();
+}
+
+DWORD WINAPI uithread(LPVOID p) {
+    CreateConsole();
+    wchar_t pyhome[] = _T("C:\\Python36-32");
+    Py_SetPythonHome(pyhome);
+    Py_Initialize();
+    PyRun_InteractiveLoop(stdin, "<stdin>");
+    return 0;
+
+}
+void  __stdcall mythread(wchar_t*) {
+    //AfxBeginThread(myproc,0,0,0,0);
+
+    CreateThread(0, 0, uithread, 0, 0, 0);
+
+
+}
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    {
+      
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
