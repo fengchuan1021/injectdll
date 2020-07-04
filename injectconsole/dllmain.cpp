bd665ad046526c52a7f8752a6396753cb199f5b3
fengchuan <1299693999@qq.com>
2020-07-04 10:35:57 +0800

    helworld

--- injectconsole/dllmain.cpp
+++ injectconsole/dllmain.cpp
@@ -3,6 +3,103 @@
 #include <iostream>
 #include<tchar.h>
 #include <Python.h>
+#include <string>
+using namespace std;
+struct WxString
+{
+    // 存字符串
+    wchar_t* buffer;
+
+    // 存字符串长度
+    int size;
+
+    // 存字符串空间
+    int capacity;
+
+    // 8个空余的0
+    char fill[8];
+};
+
+DWORD sendaddr = 0;
+ void  sendmsg(const wchar_t* pwxid, const wchar_t* pmsg) {
+    if (sendaddr == 0){
+        sendaddr= (DWORD)GetModuleHandle(L"WeChatWin.dll")+0x34CA50;
+  
+    }
+
+    wstring wsWxId = (wchar_t*)pwxid;
+    wstring wsTextMsg = (wchar_t*)pmsg;
+    WxString wxWxid = { 0 };
+    wxWxid.buffer = (wchar_t*)wsWxId.c_str();
+    wxWxid.size = wsWxId.size();
+    wxWxid.capacity = wsWxId.capacity();
+
+    // 构造消息结构
+    WxString wxTextMsg = { 0 };
+    wxTextMsg.buffer = (wchar_t*)wsTextMsg.c_str();
+    wxTextMsg.size = wsTextMsg.size();
+    wxTextMsg.capacity = wsTextMsg.capacity();
+
+    // 构造空结构
+    WxString wxNULL = { 0 };
+
+    // 构造空buffer
+    char buffer[0x738] = { 0 };
+    __asm {
+        push 0x1;
+        lea eax, wxNULL;
+        push eax;
+
+        lea ebx, wxTextMsg;
+        push ebx;
+
+        lea edx, wxWxid;
+        lea ecx, buffer;
+
+        call sendaddr;
+        add esp, 0xc;
+
+    }
+
+
+}
+ //6e72a744
+ DWORD tmpaddress = 0;
+ DWORD addressoffset = 0;
+ DWORD backaddressoffset = 0;
+ DWORD debugaddr = 0;
+ __declspec(naked) void calloutdebug() {
+     __asm {
+         pushad;
+         pushfd;
+         push[ebp - 0x40e0];
+         call debugaddr;
+         popfd;
+         popad;
+         mov ecx,dword ptr [tmpaddress];
+
+         jmp backaddressoffset;
+
+     }
+ 
+ }
+ void opendebug() {
+     DWORD base = (DWORD)GetModuleHandle(L"WeChatWin.dll");
+
+     DWORD kbase = (DWORD)GetModuleHandle(L"kernel32.dll");
+     debugaddr = (DWORD)GetProcAddress((HMODULE)kbase, "OutputDebugStringA");
+     int *p=(int *)(base + 0x6c00ab61-1788215296);
+     *p = 1;
+     int* pl = (int*)(base + 0x51E88D6C - 0x50830000);
+     *pl = 0;
+     addressoffset = base + 0x6B386F7D - 0x6a620000;
+     backaddressoffset = addressoffset + 6;
+     tmpaddress = base + 0x6e72a744 - 0x6d080000;
+     BYTE jmpcode[5] = { 0 };
+     *(DWORD*)&jmpcode[1] = (DWORD)calloutdebug - addressoffset;
+   //  WriteProcessMemory(GetCurrentProcess(), (LPVOID)addressoffset, jmpcode, 5, 0);
+
+ }
 void CreateConsole()
 {
     if (!AllocConsole()) {
@@ -35,6 +132,7 @@ void CreateConsole()
 
 DWORD WINAPI uithread(LPVOID p) {
     CreateConsole();
+   // opendebug();
     wchar_t pyhome[] = _T("C:\\Python36-32");
     Py_SetPythonHome(pyhome);
     Py_Initialize();
@@ -58,7 +156,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     {
-      
+        sendmsg(L"filehelper",L"hell");
     }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
