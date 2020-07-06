e2af586b45a8d13f0b5b73a3cf1436ed0288d8a1
fengchuan <1299693999@qq.com>
2020-07-06 17:13:15 +0800

    python inje

--- injectconsole/dllmain.cpp
+++ injectconsole/dllmain.cpp
@@ -19,6 +19,148 @@ struct WxString
     // 8个空余的0
     char fill[8];
 };
+FILETIME fCreateTime, fAccessTime, fWriteTime = {0,0};
+PyObject* pModule = NULL;
+PyObject* pFunc = NULL;
+void inipythonscript() {
+
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("a=1 if 'd:\\wechatscript' in sys.path else sys.path.append('d:\\wechatscript')");
+    if (pModule == NULL)
+        pModule = PyImport_ImportModule("msg");
+    else
+        PyImport_ReloadModule(pModule);
+    if (!pModule) {
+        cout << "get module failed" << endl;
+        return;
+    }
+    pFunc = PyObject_GetAttrString(pModule, "callbackfun");
+    if (!pFunc || !PyCallable_Check(pFunc))
+    {
+        cout << "Can't find funftion " << endl;
+        return ;
+    }
+    HANDLE hfile = CreateFileW(L"D:\\wechatscript\\msg.py",  GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hfile != INVALID_HANDLE_VALUE) {
+        GetFileTime(hfile, &fCreateTime, &fAccessTime, &fWriteTime);
+    }
+    CloseHandle(hfile);
+}
+DWORD coveraddress = 0;
+DWORD retaddress = 0;
+void hookmsg(DWORD ebp) {
+   HANDLE hfile= CreateFileW(L"D:\\wechatscript\\msg.py", GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+   if (hfile != INVALID_HANDLE_VALUE) {
+       if (fWriteTime.dwHighDateTime==0 && fWriteTime.dwLowDateTime==0) {
+           inipythonscript();
+       }
+       else {
+           FILETIME tmptime;
+           GetFileTime(hfile, &fCreateTime, &fAccessTime, &tmptime);
+           if (fWriteTime.dwHighDateTime == tmptime.dwHighDateTime && fWriteTime.dwLowDateTime == tmptime.dwLowDateTime) {
+           }
+           else {
+               inipythonscript();
+           }
+       }
+      
+
+   }
+   CloseHandle(hfile);
+   if (pFunc != NULL) {
+      // PyObject* arg1 = Py_BuildValue("i", ebp);
+       cout << "beforecall" << endl;
+       //PyObject* result= PyObject_CallObject(pFunc, NULL);
+       PyObject* result = PyObject_CallFunction(pFunc, "i",ebp);
+       
+    //  if (arg1)
+     //      Py_DECREF(arg1);
+       if (result)
+           Py_DECREF(result);
+   }
+       
+}
+_declspec(naked) void listenrecvmsg()
+{
+    
+    __asm {
+        pushad;
+        pushfd;
+        push ebp;
+        call hookmsg;
+        add esp, 0x4;
+        popfd;
+        popad;
+        push coveraddress;
+        jmp retaddress;
+
+    
+    }
+}
+
+void installhook() {
+    DWORD dllbase = (DWORD)GetModuleHandle(L"WeChatWin.dll");
+    DWORD hookoffset = 0x329743;
+    DWORD hookaddress = dllbase + hookoffset;
+     retaddress = hookaddress + 5;
+    BYTE jmpcode[5] = { 0 };
+    coveraddress = dllbase + 0x14003C0;
+    jmpcode[0] = 0xe9;
+    *(DWORD*)(&jmpcode[1]) = (DWORD)listenrecvmsg - hookaddress - 5;
+    cout << "before write memory" << endl;
+    cout << dllbase << endl << hookaddress << endl << *(DWORD*)(&jmpcode[1]) << endl;
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)hookaddress, jmpcode, 5, 0);
+
+
+}
+void getmoney(wchar_t* transferid, wchar_t* wxid)
+{
+    wstring tmpid = transferid;
+    wstring twxid = wxid;
+    std::wcout << tmpid << endl;
+    std::wcout << twxid << endl;
+    struct CllectMoneyStruct
+    {
+        wchar_t* ptransferid;
+        int transferidLen;
+        int transferidMaxLen;
+        char full[0x8] = { 0 };
+        wchar_t* pwxid;
+        int wxidLen;
+        int wxidMaxLen;
+        char full2[0x8] = { 0 };
+    };
+
+    CllectMoneyStruct cllect;
+    cllect.ptransferid = transferid;
+    cllect.transferidLen = wcslen(transferid) + 1;
+    cllect.transferidMaxLen = (wcslen(transferid) + 1) * 2;
+    cllect.pwxid = wxid;
+    cllect.wxidLen = wcslen(wxid) + 1;
+    cllect.wxidMaxLen = (wcslen(wxid) + 1) * 2;
+
+    char* asmBuff = (char*)&cllect.ptransferid;
+    DWORD dwWeChatWinAddr = (DWORD)GetModuleHandle(L"WeChatWin.dll");
+	DWORD dwCall1 = dwWeChatWinAddr +0x5c28dc90-0x5ba70000;
+	DWORD dwCall2 = dwWeChatWinAddr + 0x5c28dd10 - 0x5ba70000;
+	__asm
+	{
+		sub esp, 0x30;
+		mov ecx, esp;
+		mov eax, asmBuff;
+		push eax;
+		call dwCall1;
+		call dwCall2;
+		add esp, 0x30;
+	}
+};
+  
+
+
 
 DWORD sendaddr = 0;
  void  sendmsg(const wchar_t* pwxid, const wchar_t* pmsg) {
@@ -132,11 +274,13 @@ void CreateConsole()
 
 DWORD WINAPI uithread(LPVOID p) {
     CreateConsole();
-   // opendebug();
+    
     wchar_t pyhome[] = _T("C:\\Python36-32");
     Py_SetPythonHome(pyhome);
     Py_Initialize();
-    PyRun_InteractiveLoop(stdin, "<stdin>");
+    inipythonscript();
+    installhook();
+   // PyRun_InteractiveLoop(stdin, "<stdin>");
     return 0;
 
 }
---
+++ test/test.cpp
@@ -0,0 +1,48 @@
+﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+FILETIME fCreateTime, fAccessTime, fWriteTime = { 0,0 };
+void inipythonscript() {
+
+    HANDLE hfile = CreateFileW(L"D:\\wechatscript\\msg.py", GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hfile != INVALID_HANDLE_VALUE) {
+        cout<<"get1"<<endl;
+        auto ret=GetFileTime(hfile, &fCreateTime, &fAccessTime, &fWriteTime);
+        cout << "gtfiletm" << ret << endl;
+       cout<< GetLastError() << endl;
+    }
+    CloseHandle(hfile);
+}
+int main()
+{
+    inipythonscript();
+    cout << fWriteTime.dwHighDateTime << endl;
+    cout << fWriteTime.dwLowDateTime << endl;
+    HANDLE hfile = CreateFileW(L"D:\\wechatscript\\msg.py", GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hfile != INVALID_HANDLE_VALUE) {
+        if (fWriteTime.dwHighDateTime == 0 && fWriteTime.dwLowDateTime == 0) {
+            cout << "wy" << endl;
+
+        }
+    }
+
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
